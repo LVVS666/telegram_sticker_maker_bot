931f040102d5fa79d43e672be6e1348b4fc0e9ce
Василий Логинов <97108615+LVVS666@users.noreply.github.com>
2023-09-06 21:03:52 -0700

    work_first_button

--- bot/convert.py
+++ bot/convert.py
@@ -5,8 +5,9 @@ from bot import TEMP_FOLDER
 
 
 def convert_video(video):
-    output_video_path = os.path.join(TEMP_FOLDER, "converted_video.webm")
+    '''Функция принимает видео файл и возвращает его в формате webm с параметрами видео-стикера'''
 
+    output_video_path = os.path.join(TEMP_FOLDER, "converted_video.webm")
 
     # Команда для конвертации видео в формат WebM с VP9 кодеком и без аудио
     command = [
